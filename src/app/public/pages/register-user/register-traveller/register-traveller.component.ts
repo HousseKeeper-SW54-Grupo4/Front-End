049c77c8c17ec2cae65466ab7c243111aa8a7653
LeonardoUPC <u202017075@upc.edu.pe>
2023-09-27 02:24:52 -0500

    feat(Front-End)
    feat(Front-End) Adding register-user

---
+++ src/app/public/pages/register-user/register-traveller/register-traveller.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-register-traveller',
+  templateUrl: './register-traveller.component.html',
+  styleUrls: ['./register-traveller.component.css']
+})
+export class RegisterTravellerComponent {
+  name = '';
+  lastName= '';
+  birthdate = '';
+  phone = '';
+  email = '';
+  password = '';
+  repeat_password = '';
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigateByUrl('/login');
+  }
+  goToRegisterKeeper(){
+    this.router.navigateByUrl('/register-keeper');
+  }
+}
